b3a28e0d71cd79c41f39b25654dfeccaa4584713
ImamM5 <imamm@nycstudents.net>
2022-10-07 09:30:12 -0400

    new code

--- src/TipCalcRunner.java
+++ src/TipCalcRunner.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class TipCalcRunner {
+import java.util.Scanner;
+
+public class TipCalcRunner
+{
+    public static void main (String [] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello welcome to Tip Calculator");
+        System.out.println("-------------------------------");
+        System.out.println();
+        System.out.print("Please input the total bill: ");
+        double tBill = input.nextDouble(); //user inputs the total bill amount
+        System.out.print("Please input the tip percentage (in a whole number, without the percentage sign): ");
+        double tTip = input.nextDouble(); //user inputs the percentage amount in whole number
+        System.out.print("Please input the number of people in the party: ");
+        int people = input.nextInt(); //user inputs the total number of people in the party
+        System.out.println();
+
+        TipCalculator tipCalc = new TipCalculator(tBill, tTip, people);
+        tipCalc.setTipAmount();
+        tipCalc.setTotalBillTip();
+        tipCalc.setTipPerPerson();
+        tipCalc.setTotalPerPerson();
+
+        tipCalc.calcPrinter();
+
+    }
 }
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,75 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator
+{
+    private double tBill;
+    private double tTip;
+    private int people;
+    private double tipAmount;
+    private double tTipBill;
+    private double tipPerPerson;
+    private double tPerPerson;
+
+
+    public TipCalculator(double tBill, double tTip, int people)
+    {
+        this.tTip = tTip;
+        this.tBill = tBill;
+        this.people = people;
+
+
+    }
+
+    // sets the values
+    public void setTipAmount()
+    {
+        this.tipAmount = tBill*(tTip/100);
+    }
+
+    public void setTotalBillTip()
+    {
+        this.tTipBill = tBill + tipAmount;
+    }
+
+    public void setTipPerPerson()
+    {
+        this.tipPerPerson = (tBill/people) * (tTip/100);
+    }
+
+    public void setTotalPerPerson()
+    {
+        this.tPerPerson = (tBill/people) + tipPerPerson;
+    }
+
+    //gets the values
+    public double getTipAmount() {
+        return tipAmount;
+    }
+
+    public double getTTipBill() {
+        return tTipBill;
+    }
+
+    public double getTipPerPerson() {
+        return tipPerPerson;
+    }
+
+    public double getTPerPerson() {
+        return tPerPerson;
+    }
+
+    public void calcPrinter()
+    {
+        System.out.print("Total tip amount: ");
+        System.out.printf("%.2f", getTipAmount());
+        System.out.println();
+        System.out.print("Total bill including tip: ");
+        System.out.printf("%.2f", getTTipBill());
+        System.out.println();
+        System.out.print("Tip per person: ");
+        System.out.printf("%.2f", getTipPerPerson());
+        System.out.println();
+        System.out.print("Total per person: ");
+        System.out.printf("%.2f", getTPerPerson());
+        System.out.println();
+        System.out.println("-------------------------------");
+    }
 }
